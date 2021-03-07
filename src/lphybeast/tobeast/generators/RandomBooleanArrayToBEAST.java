f7e0a5b8884155de1820359f13ea41285d01d986
walterxie <walterxie@users.noreply.github.com>
2021-03-08 11:11:12 +1300

    remove S from beastObjecgts #29

--- src/lphybeast/tobeast/generators/RandomBooleanArrayToBEAST.java
+++ src/lphybeast/tobeast/generators/RandomBooleanArrayToBEAST.java
@@ -1,10 +1,12 @@
 package lphybeast.tobeast.generators;
 
 import beast.core.BEASTInterface;
+import beast.core.parameter.IntegerParameter;
 import beast.math.distributions.ParametricDistribution;
 import beast.math.distributions.Prior;
 import lphy.core.distributions.RandomBooleanArray;
 import lphy.graphicalModel.Generator;
+import lphy.graphicalModel.Value;
 import lphybeast.BEASTContext;
 import lphybeast.GeneratorToBEAST;
 
@@ -13,19 +15,23 @@ import java.util.Objects;
 public class RandomBooleanArrayToBEAST implements GeneratorToBEAST<RandomBooleanArray, Prior> {
     @Override
     public Prior generatorToBEAST(RandomBooleanArray generator, BEASTInterface value, BEASTContext context) {
+        // Poisson => S => RandomBooleanArray => I
+        Value s = Objects.requireNonNull(generator.getParams().get("hammingWeight"));
+        BEASTInterface sI = context.getBEASTObject(s);
+        if ( !(sI instanceof IntegerParameter) )
+            throw new IllegalStateException("Expecting the mapped BEAST IntegerParameter S from hammingWeight !");
+        // rm IntegerParameter S
+        context.removeBEASTObject(sI);
 
-//        throw new UnsupportedOperationException("in dev");
-
-        Generator poissonGenerator = Objects.requireNonNull(generator.getParams().get("hammingWeight")).getGenerator();
-
+        // Poisson
+        Generator poissonGenerator = s.getGenerator();
         Prior poissonPrior = (Prior) context.getBEASTObject(poissonGenerator);
         // need distr not prior
         context.removeBEASTObject(poissonPrior);
 
+        ParametricDistribution dist = poissonPrior.distInput.get();
         beast.core.util.Sum x = new beast.core.util.Sum();
         x.setInputValue("arg", value);
-
-        ParametricDistribution dist = poissonPrior.distInput.get();
         Prior prior = BEASTContext.createPrior(dist, x);
         prior.setID(value.getID() + ".prior");
         return prior;
