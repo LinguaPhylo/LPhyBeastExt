b2da0605b650d5031cfdb29f77555374cd91c986
walterxie <walterxie@users.noreply.github.com>
2021-08-23 15:03:46 +1200

    prepare 0.1.0

--- src/lphybeast/LPhyBEAST.java
+++ src/lphybeast/LPhyBEAST.java
@@ -25,7 +25,7 @@ import java.util.concurrent.Callable;
         "OS: ${os.name} ${os.version} ${os.arch}"})
 public class LPhyBEAST implements Callable<Integer> {
 
-    public static final String VERSION = "0.1.0 beta";
+    public static final String VERSION = "0.1.0";
 
     @Parameters(paramLabel = "LPhy_scripts", description = "File of the LPhy model specification")
     Path infile;
