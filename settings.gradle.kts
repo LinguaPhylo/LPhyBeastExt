ce0a1f8c25124cdec7adc30460a1fd72f4a769be
walterxie <walter@cs.auckland.ac.nz>
2021-10-20 11:44:21 +1300

    update settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,12 @@ rootProject.name = "LPhyBEAST"
 
 // must follow this folder structure
 var lphyPath = "../linguaPhylo"
+// this will import IntelliJ module as well
 includeBuild(lphyPath)
 // bad change of Gradle 8 to deprecate including sub-projects from outside the root project
 // watching https://github.com/gradle/gradle/issues/18644
 var lphyNameStr = "lphy"
-//includeBuild("$lphyPath/lphyNameStr")
+//includeBuild("$lphyPath/$lphyNameStr")
 include(lphyNameStr)
 project(":${lphyNameStr}").projectDir = file("${lphyPath}/${lphyNameStr}")
 
