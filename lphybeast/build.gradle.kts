3a56827680739719f8fa5f2e7e859cbdefbaa469
walterxie <walter@cs.auckland.ac.nz>
2021-10-22 15:19:41 +1300

    add distributions

--- lphybeast/build.gradle.kts
+++ lphybeast/build.gradle.kts
@@ -10,27 +10,29 @@ version = "0.2-SNAPSHOT"
 
 
 dependencies {
-    implementation(project(":lphy"))
+    implementation(project(":lphy")) // compileOnly
     //TODO
 //    implementation(files("libs/lphy-1.1-SNAPSHOT.jar"))
 //    implementation(files("$projectDir/../linguaPhylo/lphy/libs/jebl-3.0.1.jar"))
-
     implementation("com.google.guava:guava:23.6-jre")
     implementation("org.jblas:jblas:1.2.3")
     implementation("info.picocli:picocli:4.5.2")
 
     // not in maven
-    implementation(files("libs/beast-2.6.6.jar"))
-    implementation(files("libs/BEASTlabs-1.9.7.jar"))
-    implementation(files("libs/BEAST_CLASSIC.addon.v1.5.0.jar"))
-    implementation(files("libs/FastRelaxedClockLogNormal.addon.v1.1.1.jar"))
-    implementation(files("libs/SSM.v1.1.0.jar"))
+    compileOnly(files("libs/beast-2.6.6.jar"))
+//    compileOnly(project(":beast2"))
+    compileOnly(files("libs/BEASTlabs-1.9.7.jar"))
+    compileOnly(files("libs/BEAST_CLASSIC.addon.v1.5.0.jar"))
+    compileOnly(files("libs/FastRelaxedClockLogNormal.addon.v1.1.1.jar"))
+    compileOnly(files("libs/SSM.v1.1.0.jar"))
 
+    compileOnly(files("libs/feast-7.9.1.jar"))
+    compileOnly(files("libs/Mascot.v2.1.2.jar"))
+    compileOnly(files("libs/MM.addon.v1.1.1.jar"))
+    compileOnly(files("libs/SA.v2.0.2.jar"))
+    // not released
     implementation(files("libs/bdtree.jar"))
-    implementation(files("libs/feast-7.9.1.jar"))
-    implementation(files("libs/Mascot.v2.1.2.jar"))
-    implementation(files("libs/MM.addon.v1.1.1.jar"))
-    implementation(files("libs/SA.v2.0.2.jar"))
+
     //implementation(fileTree("lib") { exclude("junit-*.jar") })
 
     testImplementation("junit:junit:4.13.2")
@@ -64,6 +66,16 @@ tasks.test {
     maxHeapSize = "1G"
 
     exclude("**/Tutorial*")
+}
+
+distributions {
+    main {
+        contents {
+            from("$rootDir") {
+                include("README.md")
+            }
+        }
+    }
 }
 
 
