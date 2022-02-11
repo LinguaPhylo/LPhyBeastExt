21e41743e054001da8c6c50df600b24925c9e5e9
walterxie <walter@cs.auckland.ac.nz>
2022-02-11 16:33:58 +1300

    better distZip not using app plugin

--- lphybeast/build.gradle.kts
+++ lphybeast/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
     `java-library`
-    application
+// DO NOT use app plugin, it will mess up distZip
     distribution
     `maven-publish`
     signing
     id("io.github.linguaphylo.platforms.lphy-publish") version "0.1.1"
 }
 
-group = "io.github.linguaphylo"
 // TODO 3 versions: here, LPhyBEAST, version.xml
 // version has to be manually adjusted to keep same between version.xml and here
 version = "0.2.1"
@@ -18,9 +17,20 @@ java {
     withSourcesJar()
 }
 
-val isRuntime: String by project
 val beast2Jars = fileTree("lib2") {
     exclude("**/starbeast2-*.jar")
+}
+val notReleasedJars = fileTree("lib") {
+    // not released, so must include in lphybeast release
+    include("bdtree.jar")
+}
+val lblibs by configurations.creating {
+    // Add to defaultDependencies to get their all jars
+    defaultDependencies {
+        add(project.dependencies.implementation("com.google.guava:guava:23.6-jre"))
+        add(project.dependencies.implementation("org.jblas:jblas:1.2.3"))
+        add(project.dependencies.implementation("info.picocli:picocli:4.6.2"))
+    }
 }
 
 // if the project dependencies ues impl, then impl(proj(..)) will only have source code,
@@ -28,21 +38,14 @@ val beast2Jars = fileTree("lib2") {
 // if api is used, then all dependencies will pass to here,
 // but do not use api unless you have to.
 dependencies {
-    implementation("com.google.guava:guava:23.6-jre")
-    implementation("org.jblas:jblas:1.2.3")
-    implementation("info.picocli:picocli:4.6.2")
+//    implementation(lblibs)
 
     // io.github.linguaphylo
     implementation("io.github.linguaphylo:lphy:1.1.0")
     // all released beast 2 libs
     implementation(beast2Jars)
-
-    implementation(fileTree("lib") {
-        // non-modular lphy jar incl. all dependencies
-//        include("lphy-*-all.jar")
-        // not released, so must include in lphybeast release
-        include("bdtree.jar")
-    })
+    // other jars must be included
+    implementation(notReleasedJars)
 
     // tests
     testImplementation("junit:junit:4.13.2")
@@ -57,11 +60,6 @@ dependencies {
 }
 
 var maincls: String = "lphybeast.LPhyBEAST"
-application {
-    // use classpath, beast2 not use module
-    mainClass.set(maincls)
-}
-
 tasks.jar {
     manifest {
         // shared attr in the root build
@@ -72,28 +70,52 @@ tasks.jar {
     }
 }
 
+// launch lphybeast
+tasks.register("runLPhyBEAST", JavaExec::class.java) {
+    // use classpath
+    jvmArgs = listOf("-cp", sourceSets.main.get().runtimeClasspath.asPath)
+    println("clspath = ${sourceSets.main.get().runtimeClasspath.asPath}")
+    mainClass.set(maincls)
+    setArgs(listOf("-o", "$rootDir/tmp/RSV2.xml",
+        "$rootDir/../linguaPhylo/examples/RSV2.lphy"))
+}
+
 tasks.getByName<Tar>("distTar").enabled = false
 // exclude start scripts
-tasks.getByName<CreateStartScripts>("startScripts").enabled = false
+//tasks.getByName<CreateStartScripts>("startScripts").enabled = false
 
 // dist as a beast2 package:
-// 1. all released b2 packages are excluded;
-// 2. lphy-*-all.jar is excluded, because SPI is not working with BEAST;
-// 3. cannot use modular jar, because BEAST uses a customised non-module system.
+// 1. never use `application` Gradle plugin;
+// 2. include the task output, not the output files;
+// 3. exclude lphy from lphybeast core release, because SPI does not work with BEAST class loader.
+//    But for lphybeast extensions, lphy part has to be included, due to BEAST class loader.
 distributions {
     main {
         contents {
 //            eachFile {  println(relativePath)  }
             includeEmptyDirs = false
+            into("lib") {
+//                println(lblibs.files.toList())
+//                println(lblibs.fileCollection(project.dependencies.implementation("com.google.guava:guava:23.6-jre")).toList())
+                // all 3rd party jars
+                from(lblibs.files)
+                // lphybeast core jar
+                from(tasks.jar)
+                // bdtree
+                from(notReleasedJars)
+            }
+            into("."){
+                from("$rootDir") {
+                    include("README.md")
+                    include("LICENSE")
+                    include("version.xml")
+                }
+            }
             // include src jar
-            from(tasks.getByName<Jar>("sourcesJar")) {
-                into("src")
+            into("src") {
+                from(tasks.getByName<Jar>("sourcesJar"))
             }
-            from("$rootDir") {
-                include("README.md")
-                include("LICENSE")
-                include("version.xml")
-            }
+            // lphybeast script
             from("${layout.projectDirectory.dir("bin")}") {
                 include("lphybeast")
                 into("bin")
@@ -102,13 +124,6 @@ distributions {
                     file.setExecutable(true, true)
                 }
             }
-            // beast 2 and packages
-            exclude(beast2Jars.files.map { it.name })
-
-            // all lphy jars, SPI not working in BEAST2 class loader
-            exclude("lphy-*.jar","lphy-studio-*.jar","markdowngenerator-*.jar",
-                "ext-manager-*.jar","jebl-3.1.0.jar","commons-*.jar",
-                "antlr4-runtime-*.jar","jlatexmath-*.jar","json-*.jar")
         }
     }
 }
