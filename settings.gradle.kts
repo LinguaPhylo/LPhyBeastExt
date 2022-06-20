d07359214fc34923110ff04295592835cd480523
walterxie <walter@cs.auckland.ac.nz>
2022-06-20 16:44:40 +1200

    remove build cache and old release

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,10 @@ include("mascot")
 
 // https://docs.gradle.org/current/userguide/build_cache.html
 // https://docs.gradle.org/current/userguide/build_cache_use_cases.html
-buildCache {
-    local {
-        directory = File(rootDir, "build-cache")
-        removeUnusedEntriesAfterDays = 30
-        println("Creating local build cache : ${directory}")
-    }
-}
+//buildCache {
+//    local {
+//        directory = File(rootDir, "build-cache")
+//        removeUnusedEntriesAfterDays = 30
+//        println("Creating local build cache : ${directory}")
+//    }
+//}
